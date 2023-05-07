e711455ba93078913d9558de96a0b27f9309f67c
Dylan Molina <86082012+dylanintech@users.noreply.github.com>
2023-05-07 18:32:43 -0500

    docs are live

--- theme.config.tsx
+++ theme.config.tsx
@@ -1,17 +1,22 @@
 import React from 'react'
 import { DocsThemeConfig } from 'nextra-theme-docs'
+import styles from './components/config.module.css';
 
 const config: DocsThemeConfig = {
-  logo: <span>My Project</span>,
+  logo: 
+  <div className={styles.logoTextContainer}>
+    <img className={styles.logo} src="https://pbs.twimg.com/profile_images/1647117613321928704/ms5nHqvU_400x400.jpg" alt="flywheel logo"  />
+    <span>docs</span>
+  </div>,
   project: {
-    link: 'https://github.com/shuding/nextra-docs-template',
+    link: 'https://github.com/dylanintech/flywheel-leaderboard',
   },
   chat: {
-    link: 'https://discord.com',
+    link: 'https://discord.gg/X4Y3tGus',
   },
-  docsRepositoryBase: 'https://github.com/shuding/nextra-docs-template',
+  docsRepositoryBase: 'https://github.com/dylanintech/flywheel-docs',
   footer: {
-    text: 'Nextra Docs Template',
+    text: 'flywheel docs',
   },
 }
 
